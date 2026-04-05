2d36ed29f33e8b0af1e86c0e654095a3047097e4
Turik <turalsuleymanov2006@gmail.com>
2026-04-05 19:10:46 +0800

    Added part Alex net and Effiction net

--- efnet_active/main.py
+++ efnet_active/main.py
@@ -6,55 +6,79 @@ from pathlib import Path
 import time
 
 save_path = Path(__file__).parent
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# 1. Функция сборки AlexNet
+def build_alexnet(model_path):
+    model = torchvision.models.alexnet(weights=torchvision.models.AlexNet_Weights.IMAGENET1K_V1)
+    features = model.classifier[6].in_features
+    model.classifier[6] = torch.nn.Linear(features, 1)
+    if model_path is not None and model_path.exists():
+        model.load_state_dict(torch.load(model_path, map_location=device, weights_only=True))
+    return model.to(device).eval()
 
-def build_model(model_path):
-    weights = torchvision.models.EfficientNet_B0_Weights.IMAGENET1K_V1
-    model = torchvision.models.efficientnet_b0(weights=weights)
-
+# 2. Функция сборки EfficientNet-B0
+def build_efficientnet(model_path):
+    model = torchvision.models.efficientnet_b0(weights=torchvision.models.EfficientNet_B0_Weights.IMAGENET1K_V1)
     features = model.classifier[1].in_features
     model.classifier[1] = torch.nn.Linear(features, 1)
+    if model_path is not None and model_path.exists():
+        model.load_state_dict(torch.load(model_path, map_location=device, weights_only=True))
+    return model.to(device).eval()
 
-    if model_path == None and model_path.exists():
-        model.load_state_dict(torch.load(model_path))
-    
-    return model
 
-model_file = save_path / "model.pth"
-model = build_model(model_file)
-model.eval()
+model_alexnet = build_alexnet(save_path / "model_alexnet.pth")
+model_efnet = build_efficientnet(save_path / "model_EfficientNet.pth")
 
 transform = transforms.Compose([
     transforms.ToPILImage(),
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], 
-                         std=[0.229, 0.224, 0.225])])
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
 
-def predict(frame):
-    tensor = transform(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)).unsqueeze(0)
+
+def predict(frame, model):
+    tensor = transform(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    tensor = tensor.unsqueeze(0).to(device)
     with torch.no_grad():
-        predicted = model(tensor).squeeze()
+        predicted = model(tensor).squeeze(-1)
         prob = torch.sigmoid(predicted).item()
-    return "Person" if prob > 0.5 else "No Person", prob
+    label = "Person" if prob > 0.5 else "No Person"
+    return label, prob
 
 cap = cv2.VideoCapture(0)
 cv2.namedWindow("Camera", cv2.WINDOW_GUI_NORMAL)
 
+c = 0
+
 while True:
-    _, frame = cap.read()
-    if frame is None:
+    ret, frame = cap.read()
+    if not ret:
         break
-    
+        
     cv2.imshow("Camera", frame)
     key = cv2.waitKey(1) & 0xFF
     
     if key == ord("q"):
         break
     elif key == ord("p"):
+        c+=1
+
+        if c <= 5:
+            label_count = f"{c}/10. ДОЛЖЕН БЫТЬ ЧЕЛОВЕК "
+        elif c <= 10:
+            label_count = f"{c}/10. НЕ  ЧЕЛОВЕК ---"
+            
         t = time.time()
-        label, confidence = predict(frame)
-        print(f"Elapsed Time: {time.time() - t:.4f}s, Prediction: {label}, Confidence: {confidence:.2f}")
+        label_a, conf_a = predict(frame, model_alexnet)
+        label_e, conf_e = predict(frame, model_efnet)
+        
+        print(f"\n{label_count}")
+        print(f"Elapsed Time: {time.time() - t}s")
+        print(f"AlexNet-> {label_a} (Confidence: {conf_a})")
+        print(f"EfficientNet -> {label_e} (Confidence: {conf_e})")
+        print("-" * 60)
 
 cap.release()
 cv2.destroyAllWindows()
--- efnet_active/train_model.py
+++ efnet_active/train_model.py
@@ -13,75 +13,93 @@ save_path = Path(__file__).parent
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def build_model(model_path):
+def build_alexnet(model_path):
+    weights = torchvision.models.AlexNet_Weights.IMAGENET1K_V1
+    model = torchvision.models.alexnet(weights=weights)
+    for param in model.features.parameters():
+        param.requires_grad = False
+    features = model.classifier[6].in_features
+    model.classifier[6] = torch.nn.Linear(features, 1)
+    if model_path is not None and model_path.exists():
+        model.load_state_dict(torch.load(model_path, map_location=device, weights_only=True))
+    return model.to(device)
+
+def build_efficientnet(model_path):
     weights = torchvision.models.EfficientNet_B0_Weights.IMAGENET1K_V1
     model = torchvision.models.efficientnet_b0(weights=weights)
     for param in model.features.parameters():
         param.requires_grad = False
-
-    # features = model.classifier[6].in_features
-    # model.classifier[6] = torch.nn.Linear(features, 1)
-
     features = model.classifier[1].in_features
     model.classifier[1] = torch.nn.Linear(features, 1)
-
-    if model_path == None and model_path.exists():
-        model.load_state_dict(torch.load(model_path))
-    
+    if model_path is not None and model_path.exists():
+        model.load_state_dict(torch.load(model_path, map_location=device, weights_only=True))
     return model.to(device)
 
+model_file_a = save_path / "model_alexnet.pth"
+model_file_e = save_path / "model_EfficientNet.pth"
 
-model_file = save_path / "model.pth"
-model = build_model(model_file)
-print(model)
-
+model_a = build_alexnet(model_file_a)
+model_e = build_efficientnet(model_file_e)
 
 
 criterion = torch.nn.BCEWithLogitsLoss()
-optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, 
-                                    model.parameters()), 
-                                    lr=0.001)
+optimizer_a = torch.optim.Adam(filter(lambda p: p.requires_grad, model_a.parameters()), lr=0.001)
+optimizer_e = torch.optim.Adam(filter(lambda p: p.requires_grad, model_e.parameters()), lr=0.001)
+
 
 
 transform = transforms.Compose([
     transforms.ToPILImage(),
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], 
-                         std=[0.229, 0.224, 0.225])])
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
 
 
 def train(buffer):
     if len(buffer) < 10:
-        return None
-    model.train()
+        return None, None
+    
+    model_a.train()
+    model_e.train()
+    
     images, labels = buffer.get_batch()
-    optimizer.zero_grad()
     images = images.to(device) 
     labels = labels.to(device) 
-    predictions = model(images).squeeze()
-    loss = criterion(predictions, labels)
-    loss.backward()
-    optimizer.step()
-    return loss.item()
+    
+    # Обучение AlexNet
+    optimizer_a.zero_grad()
+    predictions_a = model_a(images).squeeze(-1)
+    loss_a = criterion(predictions_a, labels)
+    loss_a.backward()
+    optimizer_a.step()
+    
+    # Обучение EfficientNet
+    optimizer_e.zero_grad()
+    predictions_e = model_e(images).squeeze(-1)
+    loss_e = criterion(predictions_e, labels)
+    loss_e.backward()
+    optimizer_e.step()
+    
+    return loss_a.item(), loss_e.item()
 
-def predict(frame):
+def predict(frame, model):
     model.eval()
-    tensor = transform(cv2.cvtColor(frame, 
-                                    cv2.COLOR_BGR2RGB))
+    tensor = transform(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
     tensor = tensor.unsqueeze(0).to(device)
     with torch.no_grad():
-        predicted = model(tensor).squeeze()
+        predicted = model(tensor).squeeze(-1)
         prob = torch.sigmoid(predicted).item()
     label = "Person" if prob > 0.5 else "No Person"
     return label, prob
 
 class Buffer:
-    def __init__(self, maxsize = 16):
+    def __init__(self, maxsize=16):
         self.frames = deque(maxlen=maxsize)
         self.labels = deque(maxlen=maxsize)
 
-    def append(self,frame,label):
+    def append(self, frame, label):
         self.frames.append(frame)
         self.labels.append(label)
 
@@ -92,6 +110,7 @@ class Buffer:
         images = torch.stack(list(self.frames))
         labels = torch.tensor(list(self.labels), dtype=torch.float32)
         return images, labels
+
     
 
 cap = cv2.VideoCapture(0)
@@ -101,9 +120,11 @@ count_labeled = 0
 
 while True:
     _, frame = cap.read()
+
     cv2.imshow("Camera", frame)
     key = cv2.waitKey(1) & 0xFF
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
     if key == ord("q"):
         break
     elif key == ord("1"): # person
@@ -116,14 +137,17 @@ while True:
         count_labeled += 1
     elif key == ord("p"): # predict
         t= time.time()
-        label ,confidence = predict(frame)
-        print(f"Elapsed Time: {time.time() - t} seconds, Prediction: {label}, Confidence: {confidence}")
+        label_a, conf_a = predict(frame, model_a)
+        label_e, conf_e = predict(frame, model_e)
+        print(f"Elapsed: {time.time() - t}s | AlexNet: {label_a} ({conf_a}) | EffNet: {label_e} ({conf_e})")
     elif key == ord("s"): # save model
-        torch.save(model.state_dict(),save_path / "model.pth") 
+        torch.save(model_a.state_dict(), model_file_a) 
+        torch.save(model_e.state_dict(), model_file_e)
 
     print(f"Buffer Size: {len(buffer),count_labeled}")
     if count_labeled >= buffer.frames.maxlen:
+        loss_a, loss_e = train(buffer)
         loss = train(buffer)
-        if loss:
-            print(f"Loss = {loss}")
+        if loss_a is not None and loss_e is not None:
+            print(f"Loss AlexNet = {loss_a}, Loss EffNet = {loss_e}")
         count_labeled = 0
