2f5fc5aeae41c349aaf154b46bb6d714ec5ea0c9
Turik <turalsuleymanov2006@gmail.com>
2026-04-10 18:42:44 +0800

    Added all Network

--- Yolo_hw/main.py
+++ Yolo_hw/main.py
@@ -21,22 +21,46 @@ if __name__ == "__main__":
     size = "n" 
     model = YOLO("hw.pt")
 
+    
     result = model.train(
         data=str(root / "dataset.yaml"),
-        imgsz = 640,
-        batch = 640,
-        workers = 4 ,
+        imgsz=640,
+        batch=16,
+        workers=4,
+
         epochs = 10,
         patience = 5,
         optimizer = "AdamW",
         lr0 = 0.01,
         warmup_epochs = 3,
-        cos_lr = True 
+        cos_lr = True,
 
-        dropout = 0.2 
-        
-        hsv_h = 0.015
+        dropout = 0.2,
 
-        
+        hsv_h=0.015, 
+        hsv_s=0.7,
+        hsv_v=0.4,
+        flipud = 0.0,
+        fliplr = 0.5,
+        mosaic = 1.0,
+        degrees = 5.0,
+        scale = 0.5,
+        translate = 0.1,
 
+        conf = 0.01,
+        iou = 0.7,
+
+        project = "figures",
+        name = "yolo",
+        save = True,
+        save_period = 5,
+        device = 0 if torch.cuda.is_available() else "cpu",
+
+        verbose = True,
+        plots =True,
+        val = True,
+        close_mosaic = 8,
+        amp = True #FP16
     )
+    print("Done")
+    print(result.save_dir)
