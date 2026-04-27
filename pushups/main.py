a35b61f28bd0a128f300481dd9c348c8a8d861bc
Turik <turalsuleymanov2006@gmail.com>
2026-04-27 21:21:03 +0800

    Added done work

--- pushups/main.py
+++ pushups/main.py
@@ -5,7 +5,6 @@ from ultralytics.utils.plotting import Annotator
 from playsound3 import playsound
 import numpy as np
 
-
 def get_angle(a, b, c):
     cb = np.atan2(c[1] - b[1], c[0] - b[0]) 
     ab = np.atan2(a[1] - b[1], a[0] - b[0]) 
@@ -13,44 +12,49 @@ def get_angle(a, b, c):
     angle = angle + 360 if angle < 0 else angle
     return 360 - angle if angle > 180 else angle
 
-def detect_hands_up(annotated, keypoints):
-    nose_seen = (keypoints[0][0] > 0 and 
-    keypoints[0][1] > 0 )
-    
-    eyes_seen = (keypoints[1][0] > 0 and 
-    keypoints[1][1] > 0 and
-    keypoints[2][0] > 0 and
-    keypoints[2][1] > 0 )
 
+def detect_push_up(annotated, keypoints, is_down, count):
     left_shoulder = keypoints[5]
-    ritgh_shoulder = keypoints[6]
+    right_shoulder = keypoints[6]
     left_elbow = keypoints[7]
-    ritgh_elbow = keypoints[8]
+    right_elbow = keypoints[8]
     left_wrist = keypoints[9]
     right_wrist = keypoints[10]
-    if nose_seen and eyes_seen:
-        if ((left_shoulder[1] >
-            left_elbow[1] >
-            left_wrist[1]) or
-            (ritgh_shoulder[1] >
-            ritgh_elbow[1] >
-            right_wrist[1])):  
-            left_angle = get_angle(left_shoulder, left_elbow, left_wrist)   
-            cv2.putText(annotated,
-                        f"Hands up{left_angle:1f}", (10,20), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 1)
-            return True
-    return None
+    
+    trigger = False
+
+    if left_shoulder[0] > 0 and right_shoulder[0] > 0:
+        l_angle = get_angle(left_shoulder, left_elbow, left_wrist)
+        r_angle = get_angle(right_shoulder, right_elbow, right_wrist)
+
+        if l_angle < 100 and r_angle < 100 and not is_down:
+            is_down = True
+        
+        if l_angle > 150 and r_angle > 150 and is_down:
+            is_down = False
+            count += 1
+            trigger = True
+            
+        cv2.putText(annotated, f"Pushups: {count}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 3)
+
+    return is_down, count, trigger
 
 model = YOLO("yolo26n-pose.pt")
 model.to("cuda")
 
-
 camera = cv2.VideoCapture(0)
 ps = None
-sound_file = r"D:\education\CV\L12\BULLET.mp3"
+sound_file = r"D:\education\CV\HW\pushups\acolyteyes2.mp3" 
+
+is_down = False
+count = 0
+last_seen_time = time.time()
 
 while camera.isOpened():
     ret, frame = camera.read()
+    if time.time() - last_seen_time > 5.0:
+        count = 0
+        is_down = False
     cv2.imshow("Camera", frame)
     key = cv2.waitKey(10) & 0xFF
     if key == ord("q"):
@@ -58,7 +62,9 @@ while camera.isOpened():
 
     t = time.perf_counter()
     results = model.predict(frame)
-    print(f"Elapsed time {1 / (time.perf_counter() - t)}:.1f")
+    
+    
+    print(f"Elapsed time {1 / (time.perf_counter() - t):.1f}")
 
     if not results:
         continue
@@ -66,18 +72,25 @@ while camera.isOpened():
     keypoints = result.keypoints.xy.tolist()
     if not keypoints:
         continue
+        
+    if len(keypoints[0]) < 11:
+        continue
 
     print(keypoints)
-
+    last_seen_time = time.time()
     annotator = Annotator(frame)
-    annotator.kpts(result.keypoints.data[0], result.orig_shape,5,True)
+    annotator.kpts(result.keypoints.data[0], result.orig_shape, 5, True)
     annotated = annotator.result()
-    detect_hands_up(annotated, keypoints[0])
+    is_down, count, trigger = detect_push_up(annotated, keypoints[0], is_down, count)
     
-    if detect_hands_up(annotated, keypoints[0]):
+    if trigger:
         if ps is None:
-            ps = playsound(sound_file,block=False)
+            ps = playsound(sound_file, block=False)
         else:
             if not ps.is_alive():
-                ps = None
+                ps = playsound(sound_file, block=False)
+
     cv2.imshow("Pose", annotated)
+
+camera.release()
+cv2.destroyAllWindows()
